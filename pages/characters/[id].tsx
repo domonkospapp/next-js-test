f3434a437d815770512643c387545348a7afa2ee
Domonkos Papp <domonkos.papp@regnology.net>
2022-06-07 20:32:40 +0200

    Create server side rendered character page

---
+++ pages/characters/[id].tsx
@@ -0,0 +1,32 @@
+import {Character} from "../../types";
+import Image from "next/image";
+import imageLoader from "../../imageLoader";
+import {GetServerSideProps} from "next";
+import {useRouter} from "next/router";
+
+const CharacterPage = ({character}:{character: Character}) => {
+    const router = useRouter()
+    return <div>
+        <h1>{router.query.id}. {character.name}</h1>
+        <Image
+            loader={imageLoader}
+            unoptimized
+            src={character.image}
+            alt={character.name}
+            width={200}
+            height={200}
+        />
+    </div>
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${context.query.id}`)
+    const character = await res.json()
+    return {
+        props: {
+            character
+        }
+    }
+}
+
+export default CharacterPage
